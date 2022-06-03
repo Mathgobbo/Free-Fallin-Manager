b5a5d3bb4f402e17f5ecb7f8e3987980619f66ee
Matheus Gobbo Bernardi <matgobbo@hotmail.com>
2022-06-03 19:15:43 -0300

    Showing Admins

--- src/controllers/AdminListController.py
+++ src/controllers/AdminListController.py
@@ -1,12 +1,14 @@
 
+from src.dao.adminDao import AdminDao
 from src.models.Admin import Admin
 from src.views.adminListView import AdminListView
 
 
 class AdminListController:
   def __init__(self, app) -> None:
-    self.__view = AdminListView(self)
     self.__app = app
+    self.__dao = AdminDao()
+    self.__view = AdminListView(self)
 
   def openView(self):
     self.__view.show()
@@ -17,4 +19,7 @@ class AdminListController:
   def goToSignUpAdmin(self):
     self.__view.close()
     self.__app.openSignUpAdmin()
+  
+  def getAdmins(self):
+    return self.__dao.getAll()
 
--- src/views/adminListView.py
+++ src/views/adminListView.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QLineEdit, QPushButton, QLabel
+from PyQt5.QtWidgets import QMainWindow, QLineEdit, QPushButton, QLabel, QTableWidget, QTableWidgetItem
 
 from src.models.Admin import Admin
 
@@ -16,6 +16,12 @@ class AdminListView(QMainWindow):
         self.backButton.clicked.connect(self.backButtonClick)
         self.addAdminButton = self.findChild(QPushButton, "addAdminButton")
         self.addAdminButton.clicked.connect(self.openSignUpAdmin)
+        self.table = self.findChild(QTableWidget, "adminsTable")
+        self.table.setColumnWidth(0,300)
+
+        self.loadData()
+
+       
 
     def openSignUpAdminView(self):
         self.__controller
@@ -26,4 +32,13 @@ class AdminListView(QMainWindow):
 
     def openSignUpAdmin(self):
         self.__controller.goToSignUpAdmin()
+
+    def loadData(self):
+        row = 0
+        admins =  self.__controller.getAdmins()
+        self.table.setRowCount(len(admins))
+        for admin in admins:
+            self.table.setItem(row, 0, QTableWidgetItem(admin.username))
+            row = row + 1
+    
 
